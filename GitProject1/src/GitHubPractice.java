17d08c5045b7d876891abb24e4ccb12ed151c5da
ammy-hub <rathodamol3@gmail.com>
2022-09-05 19:26:31 +0530

    commit

---
+++ GitProject1/src/GitHubPractice.java
@@ -0,0 +1,22 @@
+
+public class GitHubPractice {
+
+	public static void main(String[] args) {
+		
+	  
+	
+			int a=17;
+			
+			if(a>18)
+			{
+				System.out.println("Eligible for vote");
+			}
+			else
+			{
+				System.out.println("Not Eligible for vote");
+			}
+			
+			
+	}
+
+}
---
+++ GitProject1/src/Method.java
@@ -0,0 +1,17 @@
+
+public class Method {
+
+	public static void main(String[] args) {
+		
+		int a=20;
+		int b=50;
+		int sum=0;
+		
+		sum=a+b;
+		
+		System.out.println("Sum is a & b "+ sum);
+		
+
+	}
+
+}
